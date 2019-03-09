046037f5e905ed1d34971b5572e75ed9060a3c10
Chong Guo <armourcy@gmail.com>
2019-03-09 10:08:55 -0800

    Fix(sidebar): fix nested router hover bug

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -4,9 +4,11 @@ import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    registrationOptions: { scope: './' },
     ready() {
-      console.log('Service worker is active.');
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB',
+      );
     },
     registered() {
       console.log('Service worker has been registered.');
