a22e821eee389db6af5af2737558df77bcbdecf5
xiaojianbu <xf1301695393@gmail.com>
2019-04-22 20:17:05 +0800

    Feature(Charts): add Charts Page(#26)

---
+++ src/components/Charts/mixins/resize.ts
@@ -0,0 +1,44 @@
+import { debounce } from '@/utils'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export class ResizeMixin extends Vue {
+  // eslint-disable-next-line camelcase
+  private $_sidebarElm: Element | undefined
+  private __resizeHandler: any
+  private chart: any
+
+  mounted() {
+    this.__resizeHandler = debounce(() => {
+      if (this.chart) {
+        this.chart.resize()
+      }
+    }, 100)
+    window.addEventListener('resize', this.__resizeHandler)
+    this.$_sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    this.$_sidebarElm &&
+      this.$_sidebarElm.addEventListener(
+        'transitionend',
+        this.$_sidebarResizeHandler
+      )
+  }
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.__resizeHandler)
+
+    this.$_sidebarElm &&
+      this.$_sidebarElm.removeEventListener(
+        'resize',
+        this.$_sidebarResizeHandler
+      )
+  }
+
+  // use $_ for mixins properties
+  // https://vuejs.org/v2/style-guide/index.html#Private-property-names-essential
+  // eslint-disable-next-line camelcase
+  private $_sidebarResizeHandler(e: Event) {
+    if ((e as TransitionEvent).propertyName === 'width') {
+      this.__resizeHandler()
+    }
+  }
+}
