d8853ebccd6af5dc459264ad9177cf2955d555d4
goo2821 <milk5352@gmail.com>
2020-03-21 13:58:34 +0900

    add korean

--- src/views/i18n-demo/index.vue
+++ src/views/i18n-demo/index.vue
@@ -40,6 +40,12 @@
           >
             日本語
           </el-radio>
+          <el-radio
+            label="ko"
+            border
+          >
+            한국어
+          </el-radio>
         </el-radio-group>
         <el-tag
           style="margin-top: 15px; display: block;"
@@ -205,6 +211,7 @@ export default class extends Vue {
       this.$i18n.mergeLocaleMessage('zh', local.zh)
       this.$i18n.mergeLocaleMessage('es', local.es)
       this.$i18n.mergeLocaleMessage('ja', local.ja)
+      this.$i18n.mergeLocaleMessage('ko', local.ko)
     }
     this.setOptions() // set default select options
   }
