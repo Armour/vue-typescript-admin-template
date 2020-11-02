955439d45f101a24fcb6c46d34ff50362024a74f
Chong Guo <chongg@google.com>
2020-11-02 02:33:04 +0100

    Fix(tinymce): redo changes accidentally removed by merge.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
 }
